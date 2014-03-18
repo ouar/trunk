18728bfadda5ac9b1466bbfc48c1c6e37c5ddf81
salah.ouar@gmail.com <salah.ouar@gmail.com@8a90c874-dc04-0533-cdee-ece230407e5f>
2014-03-18 17:04:06 +0000

    debut de l'implémentation du controle des login en base

--- trunk/QuizV2/Quiz-web/src/main/java/fr/gfi/quiz/presentation/authentification/Authentification.java
+++ trunk/QuizV2/Quiz-web/src/main/java/fr/gfi/quiz/presentation/authentification/Authentification.java
@@ -4,21 +4,48 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 
 import fr.gfi.quiz.metier.service.AuthentificationBS;
 
-public class Authentification implements UserDetailsService{
+public class Authentification implements AuthenticationProvider{
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Authentification.class);
+
 
 	@Resource(name="authentificationBS")
 	private AuthentificationBS authentificationBS;
 
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		List<AuthentificationBS.ROLES> lRoles = authentificationBS.authentification(username, "");
+@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		LOGGER.debug("demande d'authentification du login [" + authentication.getName() + "]");
+		String username = authentication.getName();
+		String password = authentication.getCredentials().toString();
+		List<AuthentificationBS.ROLES> lRoles = authentificationBS.authentification(username, password);
+		if(lRoles != null && lRoles.size()>0){
+			for(AuthentificationBS.ROLES role : lRoles){
+				switch(role){
+					case CANDIDAT:
+						break;
+					case ADMINISTRATEUR:
+						break;
+					case UTILISATEUR:
+						break;
+				}
+			}
+		}
+
 		return null;
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
 	}
 
 }
