2f2fbc110309cf668db7ee566c601db6780e0de1
salah.ouar@gmail.com <salah.ouar@gmail.com@8a90c874-dc04-0533-cdee-ece230407e5f>
2014-03-21 18:47:31 +0000

    correction du mapping des réponses + ajout de l'entite choix quiz

---
+++ trunk/jsonperso/src/main/java/fr/gfi/quiz/json/moteur/deserialisateur/ChoixQuizDeserialisation.java
@@ -0,0 +1,31 @@
+package fr.gfi.quiz.json.moteur.deserialisateur;
+
+import java.lang.reflect.Type;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+import fr.gfi.quiz.json.entite.ChoixQuiz;
+import fr.gfi.quiz.json.entite.IdLibelle;
+
+public class ChoixQuizDeserialisation implements JsonDeserializer<ChoixQuiz> {
+
+	@Override
+	public ChoixQuiz deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+		ChoixQuiz returnValue = new ChoixQuiz();
+		JsonObject jsonObject = json.getAsJsonObject();
+		
+		IdLibelle langage= context.deserialize(jsonObject.get("langage"), IdLibelle.class);
+		returnValue.setLangage(langage);
+		IdLibelle sujet= context.deserialize(jsonObject.get("sujet"), IdLibelle.class);
+		returnValue.setSujet(sujet);
+		IdLibelle difficulte= context.deserialize(jsonObject.get("difficulte"), IdLibelle.class);
+		returnValue.setDifficulte(difficulte);
+		
+		return returnValue;
+	}
+
+}
