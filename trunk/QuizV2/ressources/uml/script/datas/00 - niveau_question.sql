4f30d3fafee8a28a495279d90f89dd29aaeda38c
salah.ouar@gmail.com <salah.ouar@gmail.com@8a90c874-dc04-0533-cdee-ece230407e5f>
2014-03-26 17:26:38 +0000

    modif mpd

--- trunk/QuizV2/ressources/uml/script/datas/00 - niveau_question.sql
+++
@@ -1,5 +0,0 @@
-
-INSERT INTO `niveau_question` (`Id`, `lib_niveau`) VALUES
-	(1, 'facile'),
-	(2, 'moyen'),
-	(3, 'difficile');
--- trunk/QuizV2/ressources/uml/script/datas/10 - type_sujet.sql
+++ trunk/QuizV2/ressources/uml/script/datas/10 - type_sujet.sql
@@ -1,4 +1,4 @@
-INSERT INTO `type_sujet` (`Id`, `libelle`, `ref_langage`) VALUES
-	(1, 'hibernate', 1),
-	(2, 'Java/J2EE', 1),
-	(3, 'C natif', 3);
+INSERT INTO `type_sujet` (`Id`, `ref_difficulte`,`libelle`, `ref_langage`) VALUES
+	(1, 1,'hibernate', 1),
+	(2, 1,'Java/J2EE', 1),
+	(3, 1,'C natif', 3);
