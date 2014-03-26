9ed5edc2855bc2ad4429949a9dcb74ca84059764
salah.ouar@gmail.com <salah.ouar@gmail.com@8a90c874-dc04-0533-cdee-ece230407e5f>
2014-03-26 00:39:11 +0000

    changement dans la facon d'afficher les pages de quizz

--- trunk/QuizTablette/src/main/java/fr/gfi/android/quiz/model/QuizzToScreen.java
+++ trunk/QuizTablette/src/main/java/fr/gfi/android/quiz/model/QuizzToScreen.java
@@ -1,9 +1,15 @@
 package fr.gfi.android.quiz.model;
 
+import java.io.Serializable;
+
 import fr.gfi.quiz.json.entite.Quiz;
 
-public class QuizzToScreen {
+public class QuizzToScreen implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 222805380891584522L;
 	public static final int ETAT_INDETERMINE 		= 0;
 	public static final int ETAT_EN_COURS 			= 1;
 	public static final int ETAT_EN_CORRECTION 		= 2;
@@ -41,6 +47,37 @@ public class QuizzToScreen {
 	}
 	public void setQuiz(Quiz quiz) {
 		this.quiz = quiz;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + etatQuizz;
+		result = prime * result + idQuestionAffichee;
+		result = prime * result + ((quiz == null) ? 0 : quiz.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		QuizzToScreen other = (QuizzToScreen) obj;
+		if (etatQuizz != other.etatQuizz)
+			return false;
+		if (idQuestionAffichee != other.idQuestionAffichee)
+			return false;
+		if (quiz == null) {
+			if (other.quiz != null)
+				return false;
+		} else if (!quiz.equals(other.quiz))
+			return false;
+		return true;
 	}
 	
 	
