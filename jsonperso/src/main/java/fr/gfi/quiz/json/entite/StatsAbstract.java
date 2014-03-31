1000c960ecaad0cd1a2d4eb8383cabb06e405e92
salah.ouar@gmail.com <salah.ouar@gmail.com@8a90c874-dc04-0533-cdee-ece230407e5f>
2014-03-31 16:30:30 +0000

    stats ...

---
+++ jsonperso/src/main/java/fr/gfi/quiz/json/entite/StatsAbstract.java
@@ -0,0 +1,59 @@
+package fr.gfi.quiz.json.entite;
+
+public class StatsAbstract {
+	protected int nbPointsDispos=0;
+	protected int nbPointsObtenus=0;
+	protected int nbBonnesReponses=0;
+	protected int nbMauvaisesReponses=0;
+	protected int nbReponsesNonTrouvees=0;
+
+	public StatsAbstract() {
+		super();
+	}
+
+	public int getNbPointsPerdus() {
+		return nbPointsDispos-nbPointsObtenus;
+	}
+
+	public int getNbPointsDispos() {
+		return nbPointsDispos;
+	}
+
+	public void setNbPointsDispos(int nbPointsDispos) {
+		this.nbPointsDispos = nbPointsDispos;
+	}
+
+	public int getNbPointsObtenus() {
+		return nbPointsObtenus;
+	}
+
+	public void setNbPointsObtenus(int nbPointsObtenus) {
+		this.nbPointsObtenus = nbPointsObtenus;
+	}
+
+	public int getNbBonnesReponses() {
+		return nbBonnesReponses;
+	}
+
+	public void setNbBonnesReponses(int nbBonnesReponses) {
+		this.nbBonnesReponses = nbBonnesReponses;
+	}
+
+	public int getNbMauvaisesReponses() {
+		return nbMauvaisesReponses;
+	}
+
+	public void setNbMauvaisesReponses(int nbMauvaisesReponses) {
+		this.nbMauvaisesReponses = nbMauvaisesReponses;
+	}
+
+	public int getNbReponsesNonTrouvees() {
+		return nbReponsesNonTrouvees;
+	}
+
+	public void setNbReponsesNonTrouvees(int nbReponsesNonTrouvees) {
+		this.nbReponsesNonTrouvees = nbReponsesNonTrouvees;
+	}
+
+
+}
